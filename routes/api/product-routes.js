c0815b20299a208da7386e6c2d82c011e0acf8c3
JeremyF <jfulbright@gmail.com>
2022-11-18 14:50:03 -0700

    Created Product and Category Routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -59,6 +59,7 @@ router.post("/", (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -122,9 +123,24 @@ router.put("/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
-
-router.delete("/:id", (req, res) => {
+// DELETE a product
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).jason({ message: `No Product found with this id!` });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
